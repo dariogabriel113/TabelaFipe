ee41324a5c5e4ef1182c18121e7bb9ba00f60c3e
Dario Gabriel <dariogabriel113@gmail.com>
2024-06-04 16:43:34 -0300

    Inclusão da leiura do tipo de veículo

--- src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
@@ -1,7 +1,29 @@
 package br.com.alura.TabelaFipe.principal;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Principal {
+
+    private Scanner scanner = new Scanner(System.in);
+
     public void exibirMenu() {
-        System.out.println("Hello");
+        System.out.println("Digite o número do tipo de veículo que deseja consultar, (1) para carro, (2) moto ou (3) caminhão):");
+
+        try {
+            Integer valor = scanner.nextInt();
+
+            List<Integer> tipos = new ArrayList<>(Arrays.asList(1,2,3));
+
+            String msg = "Tipo não identificado, você digitou: ";
+
+            if (tipos.contains(valor)) {
+                msg = "Tipos escolhido: ";
+            }
+
+            System.out.println(msg + valor);
+        } catch(InputMismatchException inputMismatchException) {
+            System.out.println("Erro na leitura do tipo digitado.");
+        }
     }
 }
