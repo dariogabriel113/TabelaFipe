66fdab61c98663866f09b6f930f5ac9f0af8ce58
Dario Gabriel <dariogabriel113@gmail.com>
2024-06-06 14:42:22 -0300

    Separação de logica em metodos

--- src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
@@ -5,7 +5,6 @@ import br.com.alura.TabelaFipe.service.ConsumoApi;
 import br.com.alura.TabelaFipe.service.ConverteDados;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -27,34 +26,44 @@ public class Principal {
 
         try {
             String valor = scanner.nextLine();
-
-            String msg = "Tipo escolhido: ";
-            var json = "";
-
-            String tipoParaBusca = null;
-            if (valor.equalsIgnoreCase("Carro")) {
-                tipoParaBusca = "carros";
-            } else if (valor.equalsIgnoreCase("Moto")) {
-                tipoParaBusca = "motos";
-            } else if (valor.equalsIgnoreCase("Caminhão") || valor.equalsIgnoreCase("Caminhao")) {
-                tipoParaBusca = "caminhoes";
-            } else {
-                msg = "Tipo não identificado, você digitou: ";
-            }
-
-            System.out.println(msg + valor);
-            if (tipoParaBusca != null) {
-                json = consumoApi.obterDados(ENDERECO + tipoParaBusca + "/marcas");
-                System.out.println(json);
-
-                DadosVeiculo[] dados = conversor.obterDados(json, DadosVeiculo[].class);
-                Arrays.stream(dados).toList()
-                        .stream()
-                        .sorted(Comparator.comparing(DadosVeiculo::codigo))
-                        .forEach(e -> System.out.println("Código: " + e.codigo() + " - " + "Marca: " + e.nome()));
-            }
+            buscaDadosDeMarcas(valor);
         } catch (InputMismatchException inputMismatchException) {
             System.out.println("Erro na leitura do tipo digitado.");
         }
+    }
+
+    public void buscaDadosDeMarcas(String valor) {
+        var json = "";
+
+        String tipoParaBusca = buscaTipoVeiculo(valor);
+        if (tipoParaBusca != null) {
+            System.out.println("Tipo escolhido: " + valor);
+
+            json = consumoApi.obterDados(ENDERECO + tipoParaBusca + "/marcas");
+
+            DadosVeiculo[] dados = conversor.obterDados(json, DadosVeiculo[].class);
+            exibeListagemDeMarcas(dados);
+        } else {
+            System.out.println("Tipo não identificado, você digitou: " + valor);
+        }
+    }
+
+    public String buscaTipoVeiculo(String valor) {
+        if (valor.equalsIgnoreCase("Carro")) {
+            return "carros";
+        } else if (valor.equalsIgnoreCase("Moto")) {
+            return "motos";
+        } else if (valor.equalsIgnoreCase("Caminhão") || valor.equalsIgnoreCase("Caminhao")) {
+            return "caminhoes";
+        }
+
+        return null;
+    }
+
+    public void exibeListagemDeMarcas(DadosVeiculo[] dados) {
+        Arrays.stream(dados).toList()
+                .stream()
+                .sorted(Comparator.comparing(DadosVeiculo::codigo))
+                .forEach(e -> System.out.println("Cód: " + e.codigo() + " - " + "Marca: " + e.nome()));
     }
 }
